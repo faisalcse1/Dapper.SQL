19d964b599dd9f862448d8273e1866229612cd1f
Md Faisal <faisalcse1@gmail.com>
2023-11-14 11:00:21 +0600

    add IDbContext interface

---
+++ Dapper.SQL/IDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.SQL
+{
+    internal interface IDbContext
+    {
+        IEnumerable<T> Query<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<IEnumerable<T>> QueryAsync<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        T QueryFirst<T>(string query, object data = null, IDbTransaction transaction = null, int? commandTimeOut = null,
+            CommandType? commandType = null);
+
+        Task<T> QueryFirstAsync<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        T QuerySingle<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<T> QuerySingleAsync<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        T QueryFirstOrDefault<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<T> QueryFirstOrDefaultAsync<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        T QuerySingleOrDefault<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<T> QuerySingleOrDefaultAsync<T>(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        SqlMapper.GridReader QueryMultiple(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<SqlMapper.GridReader> QueryMultipleAsync(string query, object data = null,
+            IDbTransaction transaction = null, int? commandTimeOut = null, CommandType? commandType = null);
+
+        int Execute(string query, object data = null, IDbTransaction transaction = null, int? commandTimeOut = null,
+            CommandType? commandType = null);
+
+        Task<int> ExecuteAsync(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        IDataReader ExecuteReader(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        Task<IDataReader> ExecuteReaderAsync(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+
+        object ExecuteScalar(string query, object data = null, IDbTransaction transaction = null, int? commandTimeOut = null,
+            CommandType? commandType = null);
+
+        Task<object> ExecuteScalarAsync(string query, object data = null, IDbTransaction transaction = null,
+            int? commandTimeOut = null, CommandType? commandType = null);
+    }
+}
